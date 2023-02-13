e79f4f0e2f39cb084fadcdfd42f92af9640bf7ee
Deugdeugi <104217033+Deugdeugi@users.noreply.github.com>
2023-02-13 16:34:28 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,17 +1,3 @@
-import sys
-input = sys.stdin.readline
-maxValue = -1
-maxi = 0
-maxj = 0
-
-for i in range(1, 10):
-    arr = list(map(int, input().split()))
-    
-    for j in range(1, 10):
-        if arr[j - 1] > maxValue:
-            maxValue = arr[j - 1]
-            maxi = i
-            maxj = j
-
-print(maxValue)
-print(maxi, maxj)
+L=*map(int,open(0).read().split()),
+a=L.index(M:=max(L))
+print(M,a//9+1,a%9+1)
