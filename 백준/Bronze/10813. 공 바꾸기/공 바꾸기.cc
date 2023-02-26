e8d8a33a98843f3638fe180367cde9832a9784e1
Deugdeugi <104217033+Deugdeugi@users.noreply.github.com>
2023-02-26 16:01:22 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+void swap(int * a, int * b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int N = 0, M = 0, i = 0;
+    int arr[101] = { 0, };
+    int a = 0, b = 0;
+
+    for ( i == 1 ; i <= 100 ; i++ )
+    {
+        arr[i] = i;
+    }
+
+    scanf("%d %d", &N, &M);
+
+    for ( i = 0 ; i < M ; i++ )
+    {
+        scanf("%d %d", &a, &b);
+        swap(&arr[a], &arr[b]);
+    }
+
+    for ( i = 1 ; i <= N ; i++ )
+    printf("%d ", arr[i]);
+
+    return 0;
+}
